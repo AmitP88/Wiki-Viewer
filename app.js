a999059cc0bfd0cec40c9b5e77ccd0f68eec91d5
Amit Patel <amitpatel_7@msn.com>
2018-04-29 17:16:10 -0700

    slide in each .single-result div at a time on search success

--- app.js
+++ app.js
@@ -10,14 +10,21 @@ $(document).ready(function() {
           'success': function(data) {
             var list = data.query.search;
             $("#results").empty();          
-            for(var i = 0; i < list.length; i++) {
-                $("#results").append(
-                  '<div class="single-result slideRight">' +
-                    '<h3>' + list[i].title + '</h3>' +
-                    list[i].snippet +
-                    '<a href=' + 'https://en.wikipedia.org/?curid=' + list[i].pageid + ' ' + 'target=_blank' + '> See Wiki</a>' +
-                  '</div>'
-                );
+            if (list.length > 0){
+              stagger(list, 0);
+            }
+            function stagger(list, i) {
+              $("#results").append(
+                '<div class="single-result slideRight">' +
+                '<h3>' + list[i].title + '</h3>' +
+                list[i].snippet +
+                '<a href=' + 'https://en.wikipedia.org/?curid=' + list[i].pageid + ' ' + 'target=_blank' + '> See Wiki</a>' +
+                '</div>'
+              );
+              i++;
+              if (i < list.length) {
+                setTimeout(() => stagger(list, i), 1000);
+              }
             }
             // display "No results found if no wiki articles were found based on search criteria"
             if(list.length === 0){
